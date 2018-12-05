8690419b2d545579ce86525cd776d6994d5b5418
Fred Morcos <fred.morcos@gmail.com>
2018-12-05 11:48:20 +0100

    AOC2018 Day 4

--- Coding/Advent Of Code 2018/src/main.rs
+++ Coding/Advent Of Code 2018/src/main.rs
@@ -1,6 +1,10 @@
+#![warn(clippy::all)]
+
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::str::FromStr;
+use std::string::ParseError as ParseStringError;
 use std::path::Path;
 use std::fmt::Debug;
 use std::collections::{BTreeSet, HashMap as Map, HashSet as Set};
@@ -113,7 +117,10 @@ fn d2_2() {
   print!("2_2 ");
 
   if let Some((id1, id2, i)) = diff {
-    for (j, (c1, c2)) in id1.chars().zip(id2.chars()).enumerate() {
+    for (j, (c1, c2)) in id1.chars()
+                            .zip(id2.chars())
+                            .enumerate()
+    {
       if i != j {
         assert_eq!(c1, c2);
         print!("{}", c1);
@@ -139,6 +146,7 @@ struct Square {
 #[derive(Debug)]
 enum ParseError {
   ParseInt(ParseIntError),
+  ParseStr(ParseStringError),
   UnexpectedEOF,
   Other(String),
 }
@@ -146,6 +154,12 @@ enum ParseError {
 impl From<ParseIntError> for ParseError {
   fn from(e: ParseIntError) -> Self {
     ParseError::ParseInt(e)
+  }
+}
+
+impl From<ParseStringError> for ParseError {
+  fn from(e: ParseStringError) -> Self {
+    ParseError::ParseStr(e)
   }
 }
 
@@ -161,7 +175,8 @@ fn skip_ws<I>(iter: &mut Peekable<I>)
   }
 }
 
-fn single<I>(iter: &mut Peekable<I>, c: char) -> Result<(), ParseError>
+fn single<I>(iter: &mut Peekable<I>, c: char)
+             -> Result<(), ParseError>
   where I: Iterator<Item = char>,
 {
   if let Some(&c_) = iter.peek() {
@@ -177,14 +192,17 @@ fn single<I>(iter: &mut Peekable<I>, c: char) -> Result<(), ParseError>
   Ok(())
 }
 
-fn multi<T, I>(iter: &mut Peekable<I>) -> Result<T, <T as FromStr>::Err>
+fn multi<T, I, P>(iter: &mut Peekable<I>, p: P)
+                  -> Result<T, <T as FromStr>::Err>
   where T: FromStr,
         I: Iterator<Item = char>,
+        P: Fn(I::Item) -> bool,
 {
   let mut id = String::new();
 
   while let Some(&c) = iter.peek() {
-    if c.is_digit(10) {
+    // if c.is_digit(10) {
+    if p(c) {
       id.push(c);
       iter.next();
     } else {
@@ -203,26 +221,31 @@ impl FromStr for Square {
 
     single(&mut iter, '#')?;
 
-    let id = multi::<usize, _>(&mut iter)?;
+    let id = multi::<usize, _, _>
+      (&mut iter, |c| c.is_digit(10))?;
 
     skip_ws(&mut iter);
     single(&mut iter, '@')?;
     skip_ws(&mut iter);
 
-    let x = multi::<usize, _>(&mut iter)?;
+    let x = multi::<usize, _, _>
+      (&mut iter, |c| c.is_digit(10))?;
 
     single(&mut iter, ',')?;
 
-    let y = multi::<usize, _>(&mut iter)?;
+    let y = multi::<usize, _, _>
+      (&mut iter, |c| c.is_digit(10))?;
 
     single(&mut iter, ':')?;
     skip_ws(&mut iter);
 
-    let w = multi::<usize, _>(&mut iter)?;
+    let w = multi::<usize, _, _>
+      (&mut iter, |c| c.is_digit(10))?;
 
     single(&mut iter, 'x')?;
 
-    let h = multi::<usize, _>(&mut iter)?;
+    let h = multi::<usize, _, _>
+      (&mut iter, |c| c.is_digit(10))?;
 
     Ok(Square { id, x, y, w, h })
   }
@@ -281,7 +304,166 @@ fn d3_1_2() {
   }
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Mode { Sleep, Wake }
+
+#[derive(Debug, PartialEq, Eq)]
+struct Event {
+  year: usize,
+  month: usize,
+  day: usize,
+  hour: usize,
+  minute: usize,
+  guard: Option<usize>,
+  mode: Mode,
+}
+
+impl PartialOrd for Event {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.year.partial_cmp(&other.year)?.then(
+      self.month.partial_cmp(&other.month)?.then(
+        self.day.partial_cmp(&other.day)?.then(
+          self.hour.partial_cmp(&other.hour)?.then(
+            self.minute.partial_cmp(&other.minute)?)))))
+  }
+}
+
+impl Ord for Event {
+  fn cmp(&self, other: &Self) -> Ordering {
+    self.year.cmp(&other.year).then(
+      self.month.cmp(&other.month).then(
+        self.day.cmp(&other.day).then(
+          self.hour.cmp(&other.hour).then(
+            self.minute.cmp(&other.minute)))))
+  }
+}
+
+impl FromStr for Event {
+  type Err = ParseError;
+
+  fn from_str(input: &str) -> Result<Self, Self::Err> {
+    let mut iter = input.chars().peekable();
+
+    single(&mut iter, '[')?;
+    let year = multi(&mut iter, |c| c.is_digit(10))?;
+    single(&mut iter, '-')?;
+    let month = multi(&mut iter, |c| c.is_digit(10))?;
+    single(&mut iter, '-')?;
+    let day = multi(&mut iter, |c| c.is_digit(10))?;
+
+    skip_ws(&mut iter);
+
+    let hour = multi(&mut iter, |c| c.is_digit(10))?;
+    single(&mut iter, ':')?;
+    let minute = multi(&mut iter, |c| c.is_digit(10))?;
+    single(&mut iter, ']')?;
+
+    skip_ws(&mut iter);
+
+    let (guard, mode) = if let Some(&c) = iter.peek() {
+      if c == 'G' || c == 'g' {
+        multi::<String, _, _>(&mut iter, |c| !c.is_whitespace())?;
+        skip_ws(&mut iter);
+        single(&mut iter, '#')?;
+
+        (Some(multi(&mut iter, |c| c.is_digit(10))?), Mode::Wake)
+      } else if c == 'f' || c == 'F' {
+        (None, Mode::Sleep)
+      } else if c == 'w' || c == 'W' {
+        (None, Mode::Wake)
+      } else {
+        return Err(ParseError::Other(format!("Unexpected char '{}'", c)));
+      }
+    } else {
+      return Err(ParseError::UnexpectedEOF);
+    };
+
+    Ok(Event { year, month, day, hour, minute, guard, mode })
+  }
+}
+
 fn d4_1() {
+  let mut events: Vec<Event> = read_lines("day4").collect();
+  events.sort();
+
+  let mut guard: Option<usize> = None;
+  let mut mode: Mode = Mode::Wake;
+  let mut sleep_start: usize = 0;
+  let mut guards: Map<usize, Map<usize, usize>> = Map::new();
+
+  for ev in events {
+    if ev.guard.is_some() {
+      assert_eq!(ev.mode, Mode::Wake);
+      guard = ev.guard;
+      mode = Mode::Wake;
+    } else if ev.mode == Mode::Sleep {
+      assert!(guard.is_some());
+      assert_eq!(mode, Mode::Wake, "Guard: {}", guard.unwrap());
+      mode = ev.mode;
+      sleep_start = ev.minute;
+    } else if ev.mode == Mode::Wake {
+      assert!(guard.is_some());
+      assert_eq!(mode, Mode::Sleep, "Guard: {}", guard.unwrap());
+      mode = ev.mode;
+
+      for minute in sleep_start .. ev.minute {
+        let guard = guard.unwrap();
+
+        if let Some(minutes) = guards.get_mut(&guard) {
+          if let Some(count) = minutes.get_mut(&minute) {
+            *count += 1;
+          } else {
+            minutes.insert(minute, 1);
+          }
+        } else {
+          let mut minutes = Map::new();
+          minutes.insert(minute, 1);
+          guards.insert(guard, minutes);
+        }
+      }
+    }
+  }
+
+  let mut res_guard = 0;
+  let mut res_minute = 0;
+  let mut total_minutes = 0;
+
+  for (&guard, minutes) in &guards {
+    let t = minutes.values().sum();
+
+    if t > total_minutes {
+      res_guard = guard;
+      total_minutes = t;
+      res_minute = 0;
+
+      let mut minute_count = 0;
+
+      for (&minute, &count) in minutes {
+        if count > minute_count {
+          res_minute = minute;
+          minute_count = count;
+        }
+      }
+    }
+  }
+
+  println!("d4_1 ID={} * Minute={} = {}", res_guard, res_minute, res_guard * res_minute);
+
+  let mut res_guard = 0;
+  let mut res_minute = 0;
+  let mut minute_count = 0;
+
+  for (guard, minutes) in guards {
+    for (minute, count) in minutes {
+      if count > minute_count {
+        res_minute = minute;
+        minute_count = count;
+        res_guard = guard;
+      }
+    }
+  }
+
+  println!("d4_1 ID={} * Minute={} = {}", res_guard, res_minute, res_guard * res_minute);
 }
 
 fn main() {
